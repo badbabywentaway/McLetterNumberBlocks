34c0db9bb457a2c03717d44eb8c3142e0c88600f
badbabywentaway <93155351+badbabywentaway@users.noreply.github.com>
2022-04-16 15:51:26 -0400

    insert public domain dictionary, stripped of all but pure lower case words

--- src/main/java/me/stephanosbad/edublocks/mcletternumberblocks/McLetterNumberBlocks.java
+++ src/main/java/me/stephanosbad/edublocks/mcletternumberblocks/McLetterNumberBlocks.java
@@ -1,23 +1,16 @@
 package me.stephanosbad.edublocks.mcletternumberblocks;
 
 import io.th0rgal.oraxen.compatibilities.CompatibilitiesManager;
-import io.th0rgal.oraxen.items.OraxenItems;
 import me.stephanosbad.edublocks.mcletternumberblocks.Items.ItemManager;
-//import me.stephanosbad.edublocks.mcletternumberblocks.Items.GetItemCommand;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.PluginManager;
-import org.bukkit.plugin.RegisteredListener;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.Objects;
-import java.util.logging.Logger;
 
 import static org.bukkit.Bukkit.getPluginManager;
 
 public final class McLetterNumberBlocks extends JavaPlugin {
 
 
-    private boolean oraxenLoaded = false;
+    public boolean oraxenLoaded = false;
     public static Plugin oraxenPlugin;
 
 
@@ -26,10 +19,6 @@ public final class McLetterNumberBlocks extends JavaPlugin {
 
         // Plugin startup logic
         System.out.println("Minecraft Letter/Number Block Plugin Starting");
-
-        //Objects.requireNonNull(getCommand("charblock")).setExecutor(new GetItemCommand());
-        //Objects.requireNonNull(getCommand("charblock")).setTabCompleter(new GetItemCommand());
-
 
 
         if ((oraxenPlugin = getServer().getPluginManager().getPlugin("oraxen")) != null) {
